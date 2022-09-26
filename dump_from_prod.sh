3557f42a405de8ae6d14685edd956eaaef2f6cfb
Anibal <anibalsolon@gmail.com>
2022-09-26 16:55:39 -0500

    fix: some more _1

--- dump_from_prod.sh
+++ dump_from_prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #dump collections
-ssh db1 docker exec -t mongo_warehouse_1 mongodump --db=warehouse --port 27117 --collection=uis 
-ssh db1 docker exec -t mongo_warehouse_1 mongodump --db=warehouse --port 27117 --collection=datatypes
-ssh db1 docker exec -t mongo_warehouse_1 mongodump --db=warehouse --port 27117 --collection=apps
+ssh db1 docker exec -t mongo_warehouse mongodump --db=warehouse --port 27117 --collection=uis 
+ssh db1 docker exec -t mongo_warehouse mongodump --db=warehouse --port 27117 --collection=datatypes
+ssh db1 docker exec -t mongo_warehouse mongodump --db=warehouse --port 27117 --collection=apps
 
 #take it out of container
 ssh db1 rm -rf /tmp/dump #needed to prevent dump nesting inside /tmp/dump..
-ssh db1 docker cp mongo_warehouse_1:dump /tmp/dump
+ssh db1 docker cp mongo_warehouse:dump /tmp/dump
 
 #copy over here
 rsync -av db1:/tmp/dump/ dbdump
